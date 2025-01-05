a1b8fd5d50a16987fb4366ebb30cfaacb0168e9e
yuliabgkv <bugakovayk@gmail.com>
2025-01-05 15:35:19 +0200

    feat(loading-spinner): change text to animated spinner

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,7 +1,16 @@
 import classes from "./LoadingSpinner.module.css";
 
 function LoadingSpinner() {
-  return <div className={classes["loader-container"]}>Loading...</div>;
+  return (
+    <div className={classes["loader-container"]}>
+      <div className={classes["loader-ellipsis"]}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default LoadingSpinner;
